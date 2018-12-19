7b2d6fcf8a94c181e2f77d53b6b1b7cfcb1f3b98
sebastien45 <41240574+sebastien45@users.noreply.github.com>
2018-12-19 13:46:37 +0100

    upgrade LoggerUI view more details

--- R/loggerui.R
+++ R/loggerui.R
@@ -11,149 +11,12 @@
 #
 #-------------------------------------------------------------------------------
 
-#' A LoggeroldUI reference class
-#' @export LoggeroldUI
-#' @exportClass LoggeroldUI
-LoggeroldUI = setRefClass("LoggeroldUI",
-   fields = list(loglst = "LoggerList",
-                 id = "numeric",
-                 ldatestart =  "POSIXct",
-                 nbrow = "numeric"),
-   methods = list(
-     initialize = function(loglst) {
-       loglst<<-loglst
-       nbrow<<-12
-     },
-     gui = function() {
-       i=0
-       loggerchoice=list()
-       for(n in loglst$.l) {
-         i=i+1
-         loggerchoice[[n$name]]=i
-       }
-       id<<-1
-       lnbrow=loglst$.l[[1]]$nbrow
-       lbechoices=loglst$.l[[1]]$behaviorchoices
-       lbeslct=loglst$.l[[1]]$behaviorselected
-       lbecolor=loglst$.l[[1]]$becolor
-       lbechnames=list()
-       lbechvalues=list()
-       ldatestart<<-loglst$.l[[1]]$datestart
-       ui <- fluidPage(
-         sidebarLayout(
-           sidebarPanel(
-             selectInput("logger",
-                         label = "Logger:",
-                         choices = loggerchoice),
-             sliderInput("time", "RTCtick:",
-                         min = 1,
-                         max = lnbrow,
-                         value = c(min,max)),
-             actionButton("btzoom", "Zoom"),
-             actionButton("btreset", "Reset"),
-             checkboxGroupInput("checkGroup", label = "Behavior",
-                                #choices = lbechoices,
-                                #selected = lbeslct
-                                choiceNames = lbechnames,
-                                choiceValues = lbechvalues)
-           ),
-           mainPanel(
-             uiOutput("dygraph")
-           )  )
-       )
-       server <- function(input, output, session) {
-         observeEvent(input$btzoom, {
-           lmin=input$time[1]
-           lmax=input$time[2]
-           updateSliderInput(session, "time",min=lmin,max=lmax,step = 1)
-         })
-         observeEvent(input$btreset, {
-           id<<-as.numeric(input$logger)
-           lmax=loglst$.l[[id]]$nbrow
-           updateSliderInput(session, "time",min=1,max=lmax,value = c(1,lmax),step = 1)
-         })
-         observeEvent(input$logger, {
-           id<<-as.numeric(input$logger)
-           lmax=loglst$.l[[id]]$nbrow/loglst$.l[[id]]$accres
-           nbrow<<-lmax
-           updateSliderInput(session, "time",min=1,max=lmax,value=c(1,lmax))
-           lbechoices=loglst$.l[[id]]$behaviorchoices
-           lbeslct=loglst$.l[[id]]$behaviorselected
-           ldatestart<<-loglst$.l[[id]]$datestart
-           lbecolor=loglst$.l[[id]]$becolor
-           for(v in lbechoices) {
-             tag=tags$span(names(lbechoices[v]),style =paste0("color :",substr(lbecolor[v],1,7),";"))
-             lbechnames=c(lbechnames,list(tag))
-             lbechvalues=c(lbechvalues,v)
-           }
-           updateCheckboxGroupInput(session, "checkGroup",choiceNames = lbechnames, choiceValues = lbechvalues,
-                                    selected= lbeslct)
-         })
-         output$dygraph <- renderUI({
-           fres=1000
-           fmin=input$time[1]
-           fmax=input$time[2]
-           if ((fmax-fmin) < fres) {
-             fpas=1
-             fres=fmax-fmin
-           } else {
-             fpas=floor((fmax-fmin)/fres)
-           }
-           mi=seq(fmin,fmax,fpas)
-           mi=mi[1:fres]
-           fileh5=loglst$.l[[id]]$fileh5
-           f=h5file(fileh5,"r")
-           #m=ds[mi,]
-           m=f["/data"][mi,1:12]
-           h5close(f)
-           datedeb=(ldatestart+fmin)
-           datetimes <- seq.POSIXt(from=datedeb,(datedeb+fmax),fpas)
-           datetimes=datetimes[1:fres]
-           acc=cbind(m[,1],m[,2],m[,3])
-           wacc=xts(acc, order.by = datetimes, tz="GMT" )
-           dyacc=dygraphs::dygraph(wacc,main = "Accelerometer", group = "wac",height = 200) %>%
-             dyOptions(labelsUTC = TRUE)
-           lobs=loglst$.l[[id]]$beobslst
-           for( ob in lobs ) {
-             if (ob$code %in% input$checkGroup) {
-               dyacc <- dyShading(dyacc, from = ob$from , to = ob$to, color = ob$color )
-             }
-           }
-           wt=xts(m[,10], order.by = datetimes, tz="GMT" )
-           dyt=dygraphs::dygraph(wt,main = "Temperature", group = "wac",height = 200)%>%
-             dyOptions(labelsUTC = TRUE)
-           wp=xts(m[,11], order.by = datetimes, tz="GMT" )
-           dyp=dygraphs::dygraph(wp,main = "Pression", group = "wac",height = 200) %>%
-             dyOptions(labelsUTC = TRUE)
-           wl=xts(m[,12], order.by = datetimes, tz="GMT" )
-           dyl=dygraphs::dygraph(wl,main = "Light intensity", group = "wac",height = 200)%>%
-             dyOptions(labelsUTC = TRUE)
-           gyr=cbind(m[,4],m[,5],m[,6])
-           wgyr=xts(gyr, order.by = datetimes, tz="GMT" )
-           dgyr=dygraphs::dygraph(wgyr,main = "Gyroscope", group = "wac",height = 200) %>%
-             dyOptions(labelsUTC = TRUE)
-           mag=cbind(m[,7],m[,8],m[,9])
-           wmag=xts(mag, order.by = datetimes, tz="GMT" )
-           dmag=dygraphs::dygraph(wmag,main = "Magnetometer", group = "wac",height = 200) %>%
-             dyOptions(labelsUTC = TRUE)
-           dy_graph <- list(dyacc,dyt,dyp,dyl,dgyr,dmag)
-           tagList(dy_graph)
-         })
-       }
-       shinyApp(ui = ui, server = server)
-     }
-   )
-)
 
-
-
-#' A LoggerUI reference class
-#' @export LoggerUI
-#' @exportClass LoggerUI
+#' A OldLoggerUI reference class
 #' @import xts
 #' @import dygraphs
 #' @import shiny
-LoggerUI<-setRefClass("LoggerUI",
+OldLoggerUI<-setRefClass("OldLoggerUI",
   fields = list(loglst = "LoggerList",
                 id = "numeric",
                 ldatestart =  "POSIXct",
@@ -299,13 +162,13 @@ LoggerUI<-setRefClass("LoggerUI",
 )
 
 
-#' A nUI reference class
-#' @export nUI
-#' @exportClass nUI
+#' A LoggerUI reference class
+#' @export LoggerUI
+#' @exportClass LoggerUI
 #' @import xts
 #' @import dygraphs
 #' @import shiny
-nUI<-setRefClass("nUI",
+LoggerUI<-setRefClass("LoggerUI",
                       fields = list(loglst = "LoggerList",
                                     id = "numeric",
                                     ldatestart =  "POSIXct",
